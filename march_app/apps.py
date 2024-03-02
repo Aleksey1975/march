e836321c8d7ffd791287e46cdf4cdd84b9d7c336
Alex <4422504a@gmail.com>
2024-03-02 12:33:58 +0300

    'fist'

---
+++ march_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MarchAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'march_app'
---
+++ march_app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, redirect
+
+from django.http import (
+    HttpResponse, HttpResponseNotFound, Http404,
+    HttpResponseRedirect, HttpResponsePermanentRedirect)
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.template.loader import render_to_string
+from django.template.defaultfilters import slugify
+
+
+def index(request):
+    return HttpResponse("March_app")
